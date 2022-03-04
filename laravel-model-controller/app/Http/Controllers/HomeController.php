e231d69221c489de7288e682d2b3762052fe9247
neshi00 <luca.ascione85@gmail.com>
2022-03-04 16:03:27 +0100

    crea controller e recupera movies da db passsandoli alla view

---
+++ laravel-model-controller/app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller {
+    function index () {
+        $data = Movie::all();
+
+        // dd($data);
+
+
+
+        return view('home', compact("data"));
+    }
+}
